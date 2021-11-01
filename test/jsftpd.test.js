b9458e27edf885fd3b625f8734a184ffe3b35f31
Sven <mailsvb@gmail.com>
2021-11-01 23:47:48 +0100

    more tests

--- test/jsftpd.test.js
+++ test/jsftpd.test.js
@@ -1,5 +1,6 @@
 const { ftpd } = require('../index')
 const net = require('net')
+const tls = require('tls')
 const {PromiseSocket, TimeoutError} = require('promise-socket')
 
 let server
@@ -1447,6 +1448,107 @@ test('test SIZE message', async () => {
     await promiseSocket.write('SIZE mytestfile')
     content = await promiseSocket.read();
     expect(content.toString().trim()).toBe('213 15')
+
+    await promiseSocket.end()
+});
+
+test('test AUTH message', async () => {
+    const users = [
+        {
+            username: 'john',
+            allowLoginWithoutPassword: true,
+        }
+    ]
+    server = new ftpd({cnf: {port: 50021, user: users}})
+    expect(server).toBeInstanceOf(ftpd);
+    server.start()
+
+    let content
+
+    let promiseSocket = new PromiseSocket(new net.Socket())
+    let socket = promiseSocket.stream
+    await socket.connect(50021, 'localhost')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('220 Welcome')
+
+    await promiseSocket.write('AUTH NONE')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('504 Unsupported auth type NONE')
+
+    await promiseSocket.write('AUTH TLS')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('234 Using authentication type TLS')
+
+    promiseSocket = new PromiseSocket(new tls.connect({socket: socket, rejectUnauthorized: false}))
+    await promiseSocket.stream.once('secureConnect', function(){})
+
+    await promiseSocket.write('USER john')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('232 User logged in')
+
+    await promiseSocket.end()
+});
+
+test('test MLSD message over secure connection', async () => {
+    const users = [
+        {
+            username: 'john',
+            allowLoginWithoutPassword: true,
+        }
+    ]
+    server = new ftpd({cnf: {port: 50021, user: users}})
+    expect(server).toBeInstanceOf(ftpd);
+    server.start()
+
+    let content
+
+    let promiseSocket = new PromiseSocket(new net.Socket())
+    let socket = promiseSocket.stream
+    await socket.connect(50021, 'localhost')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('220 Welcome')
+
+    await promiseSocket.write('AUTH NONE')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('504 Unsupported auth type NONE')
+
+    await promiseSocket.write('AUTH TLS')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('234 Using authentication type TLS')
+
+    promiseSocket = new PromiseSocket(new tls.connect({socket: socket, rejectUnauthorized: false}))
+    await promiseSocket.stream.once('secureConnect', function(){})
+
+    await promiseSocket.write('USER john')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('232 User logged in')
+
+    await promiseSocket.write('PBSZ 0')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('200 PBSZ=0')
+
+    await promiseSocket.write('PROT P')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('200 Protection level is P')
+
+    await promiseSocket.write('EPSV')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('229 Entering extended passive mode (|||1024|)')
+
+    let promiseDataSocket = new PromiseSocket(new tls.connect(1024, 'localhost', {rejectUnauthorized: false}))
+    let dataSocket = promiseDataSocket.stream
+    await dataSocket.once('secureConnect', function(){})
+
+    await promiseSocket.write('STOR mytestfile')
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('150 Opening data channel')
+
+    await promiseDataSocket.write('SOMETESTCONTENT');
+    dataSocket.end()
+    await promiseDataSocket.end()
+
+    content = await promiseSocket.read();
+    expect(content.toString().trim()).toBe('226 Successfully transferred "mytestfile"')
 
     await promiseSocket.end()
 });
